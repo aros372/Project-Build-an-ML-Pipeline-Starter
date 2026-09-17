3b2bc298456822e56a03d7ee1d00d3357dee03f0
Ash Ross <aros372@wgu.edu>
2026-09-17 19:38:57 +0000

    completed components/test_regression_model (step 5)

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+   "test_regression_model"
 ]
 
 
@@ -119,8 +119,14 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run('components/test_regression_model', 'main',
+                parameters = {
+                    # --mlflow_model {mlflow_model} --test_dataset {test_dataset}"
+                    'mlflow_model': 'random_forest_export.csv:prod',
+                    'test_dataset': 'test_data.csv:latest'
+                }
+            )
+            
 
 
 if __name__ == "__main__":
