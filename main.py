385365a9854bedaf4193cb45065502087a3778fe
Ash Ross <aros372@wgu.edu>
2026-09-17 01:03:36 +0000

    completed basic_cleaning (step 2)

--- main.py
+++ main.py
@@ -50,10 +50,17 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run("src/basic_cleaning",
+                "main",
+                parameters={
+                    "input_artifact": f"{config['etl']['sample']}:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_data",
+                    "output_description": "pre-processed from raw_data",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
